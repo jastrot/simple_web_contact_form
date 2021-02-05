c7187a7b0f05b6adcc8a87f75b1857f2f47c57ac
Jeff Strother <support@strotherit.com>
2021-02-04 21:33:39 -0500

    Create folder structure & create inputs

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Web Contact Form</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <form action="" method="post" id="form_input">    
+        <input type="text" name="first_name" id="first_name_input">
+        <input type="text" name="last_name" id="last_name_input">
+        <input type="email" name="email" id="email_input">
+        <input type="tel" name="phone_number" id="phone_number_input">
+        <input type="number" name="phone_extension" id="phone_extension_input">
+        <button type="submit">SUBMIT</button>
+    </form>
+
+</body>
+</html>
